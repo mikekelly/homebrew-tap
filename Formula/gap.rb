1b9c9ad482d7ccfc41501ba944c2c4b326074ae7
Mike Kelly <mikekelly321@gmail.com>
2026-01-16 14:21:04 +0000

    Update gap formula SHA256 for v0.3.1 release

--- Formula/gap.rb
+++ Formula/gap.rb
@@ -7,7 +7,7 @@ class Gap < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mikekelly/gap/releases/download/v0.3.1/gap-darwin-arm64.tar.gz"
-      sha256 "c59a74dcf27b2dd4fbd0bef08fd9acf38d99c75ce3b7ac2a991ee6ac3a5abc52"
+      sha256 "6d77da7cf5c9fd0eb9b99bfa4732d3e7ddff5130313a333631f2dc3361aa3b2c"
     else
       # x86_64 binary not yet available
       odie "Intel Mac binary not yet available. Please build from source."
